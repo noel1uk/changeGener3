e55980ec9f4b242ab74491c35d0dd01240fbbcee
Noel Vock <noel1uk@gmail.com>
2018-05-17 13:45:14 +0100

    test: ensure it returns [1] when 1p change is due

--- jasmine/spec/ChangeGeneratorSpec.js
+++ jasmine/spec/ChangeGeneratorSpec.js
@@ -1,7 +1,19 @@
 describe('ChangeGenerator', function() {
 
-  it('returns an empty array when given nothing', function() {
-    cg = new ChangeGenerator();
-    expect(cg.returnChange()).toEqual([]);
+  describe('.returnChange()', function() {
+
+    it('returns an empty array when given nothing', function() {
+      cg = new ChangeGenerator();
+      expect(cg.returnChange()).toEqual([]);
+    });
+    it('returns an empty array when correct money is given', function() {
+      cg = new ChangeGenerator('1p', '1p');
+      expect(cg.returnChange()).toEqual([]);
+    });
+    it('returns an array with 1 when change of 1p is due', function() {
+      cg = new ChangeGenerator('2p', '1p');
+      expect(cg.returnChange()).toEqual([1]);
+    });
   });
+
 });
