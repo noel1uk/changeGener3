f83f0c6ba76b62e304913d049789902a2c68eda2
Noel Vock <noel1uk@gmail.com>
2018-05-17 16:38:23 +0100

    feat: returns [2, 2] when given 5p and 1p. Also refactored and now covers a large degree of test cases up until 99p

--- jasmine/src/ChangeGenerator.js
+++ jasmine/src/ChangeGenerator.js
@@ -1,17 +1,47 @@
 class ChangeGenerator {
-  constructor(moneyGiven, changeDue) {
-    if ( moneyGiven || changeDue !== undefined ) {
+  constructor(moneyGiven, itemPrice) {
+    this.changeAvailable = [2, 1];
+    if ( moneyGiven || itemPrice !== undefined ) {
       this.moneyGiven = parseInt(moneyGiven.slice(0, -1));
-      this.changeDue = parseInt(changeDue.slice(0, -1));
+      this.itemPrice = parseInt(itemPrice.slice(0, -1));
     }
+    this.changeDue = this.moneyGiven - this.itemPrice;
+    this.changePrepared = [0];
+    this.changeRemaining = this.changeDue;
   }
   returnChange() {
-    if ( this.moneyGiven === 4 ) {
-      return [2, 1];
+    if ( this.changeDue === 0 ) {
+      return this.changePrepared;
+    } 
+
+    let sumOfChangePrepared = this.changePrepared.reduce(function(accumulator, currentValue) {
+       return accumulator + currentValue;
+    });
+
+    while ( sumOfChangePrepared < this.changeDue ) {
+      if ( this.changePrepared[0] === 0 ) {
+        this.changePrepared.shift();
+      }
+      
+      for ( let i = 0, foundCoin = 0; foundCoin < 1; i++ ) {
+        if ( this.changeAvailable[i] <= this.changeRemaining ) {
+          this.changePrepared.push(this.changeAvailable[i]);
+          this.changeRemaining -= this.changeAvailable[i];
+          foundCoin = 1;
+        }
+      }
+      // this.changeAvailable.forEach(denomination => {
+      //   if (denomination <= this.changeRemaining ) {
+      //     this.changePrepared.push(denomination);
+      //     this.changeRemaining -= denomination;
+      //   }
+      // });
+      sumOfChangePrepared = this.changePrepared.reduce(function(accumulator, currentValue) {
+        return accumulator + currentValue;
+      });
     }
-    if ( this.moneyGiven === 2 ) {
-      return [1];
-    }
-    return [];
+
+    return this.changePrepared;
+
   }
 }
