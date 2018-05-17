7336f5666c799d4e2bdfd41227abf1e0b6781a90
Noel Vock <noel1uk@gmail.com>
2018-05-17 16:07:58 +0100

    refactor: let empty return value be [0] instead of []

--- jasmine/spec/ChangeGeneratorSpec.js
+++ jasmine/spec/ChangeGeneratorSpec.js
@@ -3,18 +3,18 @@ describe('ChangeGenerator', function() {
   it('changes its inputs from strings to numbers', function() {
     cg = new ChangeGenerator('1p', '1p');
     expect(cg.moneyGiven).toEqual(1);
-    expect(cg.changeDue).toEqual(1);
+    expect(cg.itemPrice).toEqual(1);
   });
 
   describe('.returnChange()', function() {
 
     it('returns an empty array when given nothing', function() {
       cg = new ChangeGenerator();
-      expect(cg.returnChange()).toEqual([]);
+      expect(cg.returnChange()).toEqual([0]);
     });
     it('returns an empty array when correct money is given', function() {
       cg = new ChangeGenerator('1p', '1p');
-      expect(cg.returnChange()).toEqual([]);
+      expect(cg.returnChange()).toEqual([0]);
     });
     it('returns an array with 1 when change of 1p is due', function() {
       cg = new ChangeGenerator('2p', '1p');
